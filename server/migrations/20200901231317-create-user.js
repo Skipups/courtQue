32b8ebe187cad7aa72c1fa793ba4f65c93cd5f67
TheLunchSpecial <choi.minuk+TheLunchSpecial@gmail.com>
2020-09-01 21:20:50 -0400

    updated code to use Sequelize to communicate with the database; added prestart to package.json to run migrations on start

---
+++ server/migrations/20200901231317-create-user.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+	    userId: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      phoneNumber: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      token:{
+        type: Sequelize.UUID,
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4
+      },    
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ server/models/user.js
@@ -0,0 +1,40 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    userId: {
+			type: DataTypes.UUID,
+			primaryKey: true,
+			defaultValue: DataTypes.UUIDV4
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		phoneNumber: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		token:{
+			type: DataTypes.UUID,
+      allowNull: false,
+			defaultValue: DataTypes.UUIDV4      
+		}    
+  }, {
+    sequelize,
+    modelName: 'Users',
+  });
+  return User;
+};

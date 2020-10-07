0eee2feda1edbbc13a98c2227f2d8a70a2251ad5
TheLunchSpecial <choi.minuk+TheLunchSpecial@gmail.com>
2020-10-07 01:29:31 -0400

    updated expo-client to display verifiation id and token from firebase.  On confirm, it will pass name, phone number, and token to COURTQUE_SERVER_NEW_USER_URL for user creation.  Also will display an Alert if the COURTQUE_SERVER_NEW_USER_URL returns http status 422.  This means the user with phone number already exists.

--- expo-client/App.js
+++ expo-client/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useRef } from 'react';
-import { Text, View, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { Alert, Text, View, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import { FirebaseRecaptchaVerifierModal } from 'expo-firebase-recaptcha';
 import axios from 'axios'
 
 import firebase from './firebase';
+import settings from './config.json';
+var querystring = require('querystring');
 
 /*
 **
@@ -12,9 +14,12 @@ import firebase from './firebase';
 **
 */
 export default function App() {
+  const [name, setName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [code, setCode] = useState('');
+
   const [verificationId, setVerificationId] = useState(null);
+  const [token, setToken] = useState(null);
 
   const recaptchaVerifier = useRef(null);
 
@@ -53,7 +58,7 @@ export default function App() {
       .then((result) => {
           result.user.getIdToken()
             .then(async token => {
-              console.log(token)
+              console.log("TOKEN : " + token)
               // fetch('http://localhost:8080/verify', {
               //   method: 'POST',
               //   headers: {
@@ -61,15 +66,61 @@ export default function App() {
               //   },
               //   body: JSON.stringify({ token })
               // })
-              const response = await axios.post('https://nameless-scrubland-96190.herokuapp.com/verify', { token })
-              console.log(response.data)
+
+              //console.log("axios.post(" + settings.COURTQUE_SERVER_NEW_USER_URL+")");
+
+              var data = {
+                name: name,
+                phoneNumber: phoneNumber,
+                verificationId: token
+              };
+
+              console.log("querystring : " + querystring.stringify(data));
+              
+              const response = await axios.post(settings.COURTQUE_SERVER_NEW_USER_URL, querystring.stringify(data), {
+                headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded'
+                }
+              })
+              .then(()=>{setToken(token);})
+              .catch(error => {
+                console.log("confirm code failed with " + error);
+                var message = "Could not confirm user - "; 
+                
+                if (error.response) {
+                  switch (error.response.status) {
+                    case 422:
+                      message += "Phone Number already confirmed";
+                      break;
+                    default:
+                      message += "error code : " + error.status;
+                      break;
+                  }
+                } else {
+                  message += "error code : " + error.status;
+                }
+
+                Alert.alert(
+                  "Confirm failed!",
+                  message,
+                  [
+                    {
+                      text:"OK"
+                    }
+                  ],
+                  {
+                    cancelable: false
+                  }
+                );
+              });
             })
             .catch(error => console.log(error))
 
         // Make Ajax call to server w token
       });
-
   }
+
+  console.log(process.env.COURTQUE_SERVER_NEW_USER_URL);
 
   return (
     <View style = { styles.container }>
@@ -77,37 +128,145 @@ export default function App() {
         ref={recaptchaVerifier}
         firebaseConfig={firebase.app().options}
       />
+      <View style={styles.fieldSet}>
+          <Text style={styles.legendRequired}
+          >* Your Name</Text>
 
-      <TextInput
-        placeholder="Phone Number"
-        onChangeText={setPhoneNumber}
-        keyboardType="phone-pad"
-        autoCompleteType="tel"
-      />
+        <TextInput
+            placeholder="Name"
+            keyboardType="ascii-capable"
+            onChangeText={setName}
+            />
+      </View>
 
-      <TouchableOpacity onPress={sendVerification}>
-        <Text>Send Verification</Text>
-      </TouchableOpacity>
+      <View style={styles.fieldSet}>
+          <Text style={styles.legendRequired}
+          >* Phone Number</Text>
+        <TextInput
+            placeholder="Phone Number"
+            keyboardType="phone-pad"
+            onChangeText={setPhoneNumber}
+            autoCompleteType="tel"
+            />
+      </View>
 
-      <TextInput
-        placeholder="Confirmation Code"
-        onChangeText={setCode}
-        keyboardType="number-pad"
-      />
+      <View style={styles.row}>
+        <TouchableOpacity
+          style={styles.button} 
+          onPress={sendVerification}>
+          <Text 
+            style={styles.buttonText}
+            >Send Verification</Text>
+        </TouchableOpacity>
+      </View>
 
-      <TouchableOpacity onPress={confirmCode}>
-        <Text>Confirm Code</Text>
-      </TouchableOpacity>
+      <View style={styles.fieldSet}>
+          <Text
+            style={styles.legendReadOnly}
+            >Verification ID</Text>
+          <Text>
+            {verificationId === null || verificationId.trim() === "" ? "N/A" : verificationId.trim()}
+          </Text> 
+      </View>
+
+      <View style={styles.fieldSet}>
+        <Text
+          style={styles.legendRequired}
+          >* Confirmation Code</Text>
+        <TextInput
+          style={styles.fieldName}
+          placeholder="Confirmation Code"
+          onChangeText={setCode}
+          keyboardType="number-pad"
+        />
+      </View>
+
+      <View style={styles.row}>
+        <TouchableOpacity
+          style={styles.button} 
+          onPress={confirmCode}>
+          <Text
+            style={styles.buttonText}>Confirm Code</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.fieldSet}>
+          <Text
+            style={styles.legendReadOnly}
+            >Token</Text>
+          <Text>
+            {token === null || token.trim() === "" ? "N/A" : token.trim()}
+          </Text> 
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    height: '100%',
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center"
+  },
+  row: {
+    padding: 5,
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  button: {
+    padding: 5,
+    borderColor: "#000000",
+    borderWidth: 1,
+    fontWeight: "bold",
+    fontSize: 24,
+    borderRadius: 5
+  },
+  buttonText: {
+    fontWeight: "bold",
+    fontSize: 24,
+  },
+  fieldSet:{
+    marginTop: 14,
+    marginLeft: 5,
+    marginRight: 5,
+    marginBottom: 5,
+    paddingHorizontal: 10,
+    paddingBottom: 10,
+    paddingTop:11,
+    borderRadius: 5,
+    borderWidth: 1,
     alignItems: 'center',
-    justifyContent: 'space-around'
-  }
+    borderColor: '#000',
+
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  legendRequired:{
+      position: 'absolute',
+      top: -15,
+      left: 10,
+      paddingTop: 3,
+      paddingBottom: 3,
+      paddingLeft: 6,
+      paddingRight: 6,
+      backgroundColor: "#0073bd",
+      color: "#ffffff",
+      fontWeight: "bold",
+      borderRadius: 5
+    },
+    legendReadOnly:{
+      position: 'absolute',
+      top: -15,
+      left: 10,
+      paddingTop: 3,
+      paddingBottom: 3,
+      paddingLeft: 6,
+      paddingRight: 6,
+      backgroundColor: "#595959",
+      color: "#ffffff",
+      fontWeight: "bold",
+      borderRadius: 5
+    }
 })
 
 
--- expo-client/firebase.js
+++ expo-client/firebase.js
@@ -1,18 +1,9 @@
 import * as firebase from 'firebase';
 import '@firebase/auth';
+import settings from './config.json';
 
   // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyB0xI4bK5AKUZm5_X6Wy0trUxkDa8tRQ_c",
-    authDomain: "court-queue-prototype.firebaseapp.com",
-    databaseURL: "https://court-queue-prototype.firebaseio.com",
-    projectId: "court-queue-prototype",
-    storageBucket: "court-queue-prototype.appspot.com",
-    messagingSenderId: "676437752698",
-    appId: "1:676437752698:web:eeb4013a8a7ea96f7e74c3",
-    measurementId: "G-ZCGDRLNFGL"
-  };
- 
+  const firebaseConfig = settings.FIREBASE_CONFIG; 
 
 firebase.initializeApp(firebaseConfig);
 export default firebase;
